e5bf4b657e44556b3a7ed9989e7414fc9467918e
Martin Haesler <martin.haesler@gmail.com>
2013-12-20 17:13:28 +0000

    Add macro for page-teplage.

--- clblog.lisp
+++ clblog.lisp
@@ -5,6 +5,9 @@
 
 (in-package #:clblog)
 
+;;; set the doctype string at the start of a HTML page to 'html5':
+(setf (html-mode) :html5)
+
 ;; two macros to spare myself of repeati ng *standard-output* nil all
 ;; the time.
 (defmacro with-html (&body body)
@@ -14,6 +17,22 @@
 (defmacro with-html-string (&body body)
   `(with-html-output-to-string (*standard-output* nil)
      ,@body))
+
+
+(defmacro page-template ((&key title) &body body)
+  "as all the blog pages use the same css files
+   use a template to for the page-headers."
+  `(with-html-string
+       (:html
+	(:head
+	 (:title ,title))
+	(:body ,@body))))
+
+
+
+
+
+
 
 
 
