b50567c833cb1f74b8cfeb6c418a07cf017626cd
Martin Haesler <martin.haesler@gmail.com>
2013-12-20 18:45:19 +0000

    Add elephant sqlite3 db.

--- clblog.lisp
+++ clblog.lisp
@@ -1,14 +1,14 @@
-(ql:quickload '(:hunchentoot :cl-who))
+(ql:quickload '(:hunchentoot :cl-who :elephant))
 
 (defpackage #:clblog
-  (:use :cl :hunchentoot :cl-who))
+  (:use :cl :hunchentoot :cl-who :elephant))
 
 (in-package #:clblog)
 
 ;;; set the doctype string at the start of a HTML page to 'html5':
 (setf (html-mode) :html5)
 
-;; two macros to spare myself of repeati ng *standard-output* nil all
+;; two macros to spare myself of repeating *standard-output* nil all
 ;; the time.
 (defmacro with-html (&body body)
     `(with-html-output (*standard-output* nil)
@@ -18,6 +18,9 @@
   `(with-html-output-to-string (*standard-output* nil)
      ,@body))
 
+;; establish a db to store 
+(defparameter *posts-db*
+  (open-store '(:clsql (:sqlite3 "clblog.db"))))
 
 (defmacro page-template ((&key title) &body body)
   "as all the blog pages use the same css files
@@ -27,6 +30,8 @@
 	(:head
 	 (:title ,title))
 	(:body ,@body))))
+
+
 
 
 
